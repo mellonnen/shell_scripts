023e95338bfe287fce6bb9c4473d226ad118d7c1
mellonen <arvidgotthard98@gmail.com>
2020-10-25 23:27:58 +0100

    set cd_with_fzf preview as default

--- cd_with_fzf.sh
+++ cd_with_fzf.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 cd_with_fzf() {
-    cd $HOME && cd "$(fd -t d -E "anaconda3" | fzf --preview="tree -C -L 2 {}" --bind="space:toggle-preview" --preview-window=:hidden)"
+    cd $HOME && cd "$(fd -t d -E "anaconda3" | fzf --preview="tree -C -L 2 {}" --bind="space:toggle-preview")"
 }
