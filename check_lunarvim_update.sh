cccae7582536c6c081d45702c4bf6a0ac1f10a55
mellonnen <arvidgotthard98@gmail.com>
2021-07-19 23:18:43 +0200

    lunarvim update checker

---
+++ check_lunarvim_update.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# checks if the LunarVim config has updates
+check_lunarvim_update () {
+  tput civis
+  git -C ~/.config/nvim fetch 2> /dev/null & #run fetch in background 
+
+  PID=$! #Get process ID
+
+  # Lunar spinner!
+  spin="🌑🌒🌓🌔🌕🌖🌗🌘"
+  i=0
+  while kill -0 $PID 2> /dev/null;
+  do
+    i=$(( (i+1) %8 ))
+    printf "\rChecking for LunarVim updates ${spin:$i:1}"
+    sleep .05
+  done
+
+  STATUS=$(git -C ~/.config/nvim status)
+  MESSAGE=$(echo "$STATUS" | awk 'FNR==2{ print }')
+
+  # check if you are behind latest commit
+  if [[ $MESSAGE =~ "behind" ]]; then
+    printf "\rUpdates for LunarVim available! "
+  else
+    printf "\rNo LunarVim updates are available."
+  fi
+  tput cnorm
+}
