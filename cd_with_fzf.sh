90ff98908c5b8838ce1233477b64258b085715b6
mellonnen <arvidgotthard98@gmail.com>
2022-12-01 18:25:42 +0100

    cd_with_fzf

--- cd_with_fzf.sh
+++ cd_with_fzf.sh
@@ -3,5 +3,5 @@
 # SOURCE: https://www.youtube.com/watch?v=QeJkAs_PEQQ&t=108s
 # Fast cd using fzf
 cd_with_fzf() {
-    cd $HOME && cd "$(fd -t d -E "go" | fzf --preview="tree -C -L 2 {}" --bind="space:toggle-preview")"
+  cd "$(fd . $HOME -t d -E "go" -E "Library" | fzf --preview="tree -C -L 2 {}" --bind="space:toggle-preview" --scheme="path" --border)"
 }
